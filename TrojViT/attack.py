3d6fd5bbb4c473d01d70e5a530206dba0ffe4852
Jack Sanderson <jackmsanderson@gmail.com>
2025-06-12 19:29:00 -0500

    update

--- TrojViT/attack.py
+++ TrojViT/attack.py
@@ -83,7 +83,7 @@ def main():
 
     # --- Load Dataset ---
     print("Loading and preprocessing dataset...")
-    dataset = load_dataset("tanganke/eurosat", "rgb")
+    dataset = load_dataset("tanganke/eurosat", "default")
 
     def transform(examples):
         inputs = image_processor(images=examples['image'], return_tensors="pt")
